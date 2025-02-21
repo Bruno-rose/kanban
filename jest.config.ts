3d9ca28ec89e2732189550cef63c4a7a842c0102
Bruno <bruno.rodriguez.sep@gmail.com>
2025-02-21 12:16:33 -0600

    test: add jest

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    // Handle module aliases (if you're using them)
+    '^@/(.*)$': '<rootDir>/src/$1',
+    // Handle CSS imports (if you're using CSS modules)
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      useESM: true,
+    }],
+  },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  // Add support for ECMAScript Modules
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+};
+
+export default config;
